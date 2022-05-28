a8b28a4f8eabfefa084f4383a19e5be12d09c035
LWJerri <CLWJerri@yandex.ua>
2022-05-28 13:25:35 +0000

    Main frontend work files

---
+++ apps/frontend/src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { Route, Router } from "svelte-navigator";
+	import Main from "./pages/Main.svelte";
+	import Upload from "./pages/Upload.svelte";
+
+	import { SvelteToast } from "@zerodevx/svelte-toast";
+</script>
+
+<Router>
+	<main>
+		<SvelteToast />
+
+		<Route path="/upload" component={Upload} />
+		<Route component={Main} />
+	</main>
+</Router>
