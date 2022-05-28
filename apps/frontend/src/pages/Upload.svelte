2f69872df2c35c30cc939f71ba6698e87adb6c32
LWJerri <CLWJerri@yandex.ua>
2022-05-28 13:25:23 +0000

    Add Upload page

---
+++ apps/frontend/src/pages/Upload.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Navbar from "../components/Navbar.svelte";
+</script>
+
+<div>
+	<Navbar />
+
+	<form class="my-5" action="/api/file" method="post" accept-charset="UTF-8" enctype="multipart/form-data">
+		<div class="flex justify-center">
+			<div class="w-96">
+				<input class="form-control block w-full text-white rounded" type="file" name="files" multiple required />
+			</div>
+
+			<button type="submit" class="ml-2 btn btn-sm btn-outline btn-error">Загрузить</button>
+		</div>
+	</form>
+</div>
