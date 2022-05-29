b9fe1f740dac63aa1e582d8ee2b44d30fb68656b
LWJerri <CLWJerri@yandex.ua>
2022-05-29 11:24:48 +0000

    Add nodemon config and apply prettier rules

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -4,50 +4,50 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import fastifyModule from "fastify";
-import fileUpload from "fastify-file-upload";
+import fastifyUpload from "fastify-file-upload";
 import { fastifyStatic } from "@fastify/static";
 import indexApi from "./routes/api";
-import { PrismaClient } from "@prisma/client";
 import indexApiDelete from "./routes/fileDelete";
 import indexApiList from "./routes/fileList";
 import indexApiUpload from "./routes/fileUpload";
+import { PrismaClient } from "@prisma/client";
 import path from "path";
 
 export const prisma = new PrismaClient();
 export const fastify = fastifyModule({ logger: true });
 
 fastify
-	.register(fastifyStatic, {
-		wildcard: false,
-		root: path.resolve(__dirname, "..", "..", "..", "apps", "frontend", "dist"),
-	})
-	.register(fileUpload)
-	.register(
-		(instance, opts, next) => {
-			instance
-				.get("/", indexApi as any)
-				.delete("/file", indexApiDelete as any)
-				.get("/file", indexApiList as any)
-				.post("/file", indexApiUpload as any);
+  .register(fastifyStatic, {
+    wildcard: false,
+    root: path.resolve(__dirname, "..", "..", "..", "apps", "frontend", "dist"),
+  })
+  .register(fastifyUpload)
+  .register(
+    (instance, opts, next) => {
+      instance
+        .get("/", indexApi as any)
+        .delete("/file", indexApiDelete as any)
+        .get("/file", indexApiList as any)
+        .post("/file", indexApiUpload as any);
 
-			next();
-		},
-		{ prefix: "/api" },
-	)
-	.get("*", function (req, reply) {
-		reply.sendFile("index.html");
-	});
+      next();
+    },
+    { prefix: "/api" },
+  )
+  .get("*", function (req, reply) {
+    reply.sendFile("index.html");
+  });
 
 boot();
 
 async function boot() {
-	try {
-		console.log("Starting...");
+  try {
+    console.log("[!]: Starting...");
 
-		await fastify.listen(3005, "0.0.0.0");
+    await fastify.listen(3005, "0.0.0.0");
 
-		console.log("Application ready!");
-	} catch (err) {
-		console.error("Can't boot backend!", err);
-	}
+    console.log("[!]: MyStorage application ready!");
+  } catch (err) {
+    console.error("[!]: Can't boot application!", err);
+  }
 }
