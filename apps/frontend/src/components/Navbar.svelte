ab9bde1e7d110714d2c37e457a53f7f2a3f7a965
LWJerri <CLWJerri@yandex.ua>
2022-05-29 11:25:29 +0000

    Apple new prettier rules for pages and components

--- apps/frontend/src/components/Navbar.svelte
+++ apps/frontend/src/components/Navbar.svelte
@@ -1,9 +1,9 @@
 <div class="navbar text-white bg-base-300 shadow-lg select-none">
-	<div class="navbar-start">
-		<a href="/" class="ml-2 font-bold text-xl outline-none">SimonStorage</a>
-	</div>
+  <div class="navbar-start">
+    <a href="/" class="ml-2 font-bold text-xl outline-none">MyStorage</a>
+  </div>
 
-	<div class="navbar-end">
-		<a class="btn btn-outline btn-success rounded md:btn-wide" href="/upload">Загрузить</a>
-	</div>
+  <div class="navbar-end">
+    <a class="btn btn-outline btn-success rounded md:btn-wide" href="/upload">Загрузить</a>
+  </div>
 </div>
--- apps/frontend/src/pages/Upload.svelte
+++ apps/frontend/src/pages/Upload.svelte
@@ -1,17 +1,17 @@
-<script>
-	import Navbar from "../components/Navbar.svelte";
+<script lang="ts">
+  import Navbar from "../components/Navbar.svelte";
 </script>
 
 <div>
-	<Navbar />
+  <Navbar />
 
-	<form class="my-5" action="/api/file" method="post" accept-charset="UTF-8" enctype="multipart/form-data">
-		<div class="flex justify-center">
-			<div class="w-96">
-				<input class="form-control block w-full text-white rounded" type="file" name="files" multiple required />
-			</div>
+  <form class="my-5" action="/api/file" method="post" accept-charset="UTF-8" enctype="multipart/form-data">
+    <div class="flex justify-center">
+      <div class="w-96">
+        <input class="form-control block w-full text-white rounded" type="file" name="files" multiple required />
+      </div>
 
-			<button type="submit" class="ml-2 btn btn-sm btn-outline btn-error">Загрузить</button>
-		</div>
-	</form>
+      <button type="submit" class="ml-2 btn btn-sm btn-outline btn-error">Загрузить</button>
+    </div>
+  </form>
 </div>
