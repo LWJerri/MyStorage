d2409e86ddab7e01c27dc95f5762ae76b5c1c0d4
LWJerri <CLWJerri@yandex.ua>
2022-05-31 23:22:43 +0000

    Update Upload page

--- apps/frontend/src/pages/Upload.svelte
+++ apps/frontend/src/pages/Upload.svelte
@@ -1,17 +1,25 @@
 <script lang="ts">
   import Navbar from "../components/Navbar.svelte";
+
+  const API_URL = "https://mystorage-adapter.lwjerri.ml/";
 </script>
 
 <div>
   <Navbar />
 
-  <form class="my-5" action="/api/file" method="post" accept-charset="UTF-8" enctype="multipart/form-data">
+  <form class="my-5" action={API_URL} method="post" accept-charset="UTF-8" enctype="multipart/form-data">
     <div class="flex justify-center px-1">
       <div class="w-96">
-        <input class="form-control block w-full text-white rounded" type="file" name="files" multiple required />
-      </div>
+        <input class="form-control block w-full text-white rounded my-1" type="file" name="files" multiple required />
+        <input
+          type="text"
+          value={document.baseURI.replace("/upload", "/api/file/")}
+          class="input input-bordered w-full rounded my-1"
+          disabled
+        />
 
-      <button type="submit" class="ml-2 btn btn-sm btn-outline btn-error">Загрузить</button>
+        <button type="submit" class="btn btn-sm w-full my-1 btn-outline btn-error">Загрузить</button>
+      </div>
     </div>
   </form>
 </div>
