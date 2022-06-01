e88a97a0a9e838c0b621a865f71b70569ed353e8
LWJerri <CLWJerri@yandex.ua>
2022-06-01 13:23:04 +0000

    Remove unused input

--- apps/frontend/src/pages/Upload.svelte
+++ apps/frontend/src/pages/Upload.svelte
@@ -9,13 +9,6 @@
     <div class="flex justify-center px-1">
       <div class="w-96">
         <input class="form-control block w-full text-white rounded my-1" type="file" name="files" multiple required />
-        <input
-          type="text"
-          value={document.baseURI.replace("/upload", "/api/file/")}
-          class="input input-bordered w-full rounded my-1"
-          name="postURL"
-          hidden
-        />
 
         <button type="submit" class="btn btn-sm w-full my-1 btn-outline btn-error">Загрузить</button>
       </div>
