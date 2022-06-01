2372a310d68c62ce725a0b8abb601a6d20da422a
LWJerri <CLWJerri@yandex.ua>
2022-06-01 11:27:51 +0000

    Add support for stream files

--- apps/backend/src/routes/fileUpload.ts
+++ apps/backend/src/routes/fileUpload.ts
@@ -1,3 +1,5 @@
+import { Base64Encode } from "base64-stream";
+import BusBoy from "busboy";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { prisma } from "../app";
 import { File } from "../helpers/interface";
@@ -7,36 +9,62 @@ export default async function (
   req: FastifyRequest & { body: { files: File | File[] }; query: { name: string } },
   res: FastifyReply,
 ) {
-  console.log(req.body, req.query.name);
-  /*try {
-    const { files } = req.body;
+  try {
+    console.log("[!]: Received POST /api/file request!");
+    let START_UPLOAD_TIME: number;
 
-    if (!files) return await res.status(404).send({ error: true });
+    const busboy = BusBoy({ headers: req.raw.headers });
 
-    const fileList = Array.isArray(files) ? files : [files];
+    let preBase64 = {
+      mimeType: "",
+      filename: "",
+      base64: "",
+    };
 
-    await Promise.all(
-      fileList.map(async (file) => {
-        const uploadResult = await S3.upload({
-          Bucket: process.env.S3_BUCKET_NAME,
-          Key: `${Date.now()}-${file.name}`,
-          Body: file.data,
-          ACL: "public-read",
-          ContentType: file.mimetype,
-        }).promise();
+    busboy.on("file", (name, file, info) => {
+      START_UPLOAD_TIME = Date.now();
 
-        await prisma.upload.create({
-          data: { name: file.name, size: file.size, url: uploadResult.Location, key: uploadResult.Key },
-        });
+      preBase64.filename = info.filename;
+      preBase64.mimeType = info.mimeType;
 
-        return uploadResult;
-      }),
-    );
+      const base64Data = new Base64Encode({ encoding: "base64" }).on("data", (data) => {
+        preBase64.base64 += data;
+      });
 
-    return await res.redirect(302, "/");
+      file.pipe(base64Data);
+    });
+
+    busboy.on("close", async () => {
+      console.log("[!]: Done! Time for proccesing request took", Date.now() - START_UPLOAD_TIME);
+
+      const finalBase64 = `data:${preBase64.mimeType};base64,${preBase64.base64.replace(/undefined/g, "")}`;
+      const uploadData = Buffer.from(finalBase64.replace(/^data:image\/\w+;base64,/, ""), "base64");
+
+      const uploadResult = await S3.upload({
+        Bucket: process.env.S3_BUCKET_NAME,
+        Key: `${Date.now()}-${preBase64.filename}`,
+        Body: uploadData,
+        ACL: "public-read",
+        ContentType: preBase64.mimeType,
+      }).promise();
+
+      await prisma.upload.create({
+        data: {
+          name: preBase64.filename,
+          size: Buffer.from(finalBase64, "base64").length,
+          url: uploadResult.Location,
+          key: uploadResult.Key,
+        },
+      });
+
+      res.raw.writeHead(303, { Connection: "close", Location: "/" });
+      res.raw.end();
+    });
+
+    req.raw.pipe(busboy);
   } catch (err) {
     console.error("fileUpload error:", err);
 
     return await res.status(503).send({ error: true });
-  }*/
+  }
 }
