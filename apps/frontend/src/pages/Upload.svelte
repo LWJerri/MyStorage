caa61613325223ecbc348885a6e6a143e31d156a
LWJerri <CLWJerri@yandex.ua>
2022-06-01 10:31:52 +0000

    Change post request to `/api/file`

--- apps/frontend/src/pages/Upload.svelte
+++ apps/frontend/src/pages/Upload.svelte
@@ -1,13 +1,11 @@
 <script lang="ts">
   import Navbar from "../components/Navbar.svelte";
-
-  const API_URL = "https://mystorage-adapter.lwjerri.ml/";
 </script>
 
 <div>
   <Navbar />
 
-  <form class="my-5" action={API_URL} method="post" accept-charset="UTF-8" enctype="multipart/form-data">
+  <form class="my-5" action="/api/file" method="post" accept-charset="UTF-8" enctype="multipart/form-data">
     <div class="flex justify-center px-1">
       <div class="w-96">
         <input class="form-control block w-full text-white rounded my-1" type="file" name="files" multiple required />
