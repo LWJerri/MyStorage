d852e518a3375c1373e5da7c4c9900245f54c7a1
LWJerri <CLWJerri@yandex.ua>
2022-06-03 00:11:01 +0000

    Add `/api/` proxy

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -3,4 +3,14 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
   plugins: [svelte()],
+  server: {
+    port: 3001,
+    proxy: {
+      "/api": {
+        target: process.env.VITE_API_URL ?? "http://localhost:3005",
+        changeOrigin: true,
+      },
+    },
+  },
+  optimizeDeps: { exclude: ["svelte-routing", "js-cookie"] },
 });
