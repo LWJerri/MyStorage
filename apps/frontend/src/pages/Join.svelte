906b79becd012459533208c8bf659cc33f0b109a
LWJerri <CLWJerri@yandex.ua>
2022-06-06 16:07:46 +0000

    Add interfaces and rewrite login/register checks

--- apps/frontend/src/pages/Join.svelte
+++ apps/frontend/src/pages/Join.svelte
@@ -1,32 +1,55 @@
 <script lang="ts">
-  import cookie from "js-cookie";
+  import { toast } from "@zerodevx/svelte-toast";
+  import { toastInfo } from "../helpers/toasts";
+
+  interface RegisterOrLogin {
+    password: string;
+    username: string;
+    accessKey: string;
+    secretKey: string;
+    bucket: string;
+    admin: string;
+    endpoint: string;
+  }
+
+  interface Error {
+    error: boolean;
+    text?: string;
+  }
 
   let isRegister: boolean = false;
 
-  let password: string;
-  let username: string;
-  let accessKey: string;
-  let secretKey: string;
-  let bucket: string;
-  let admin: string;
-  let endpoint: string;
-
   let error = {
-    active: false,
+    error: false,
     text: "",
-  } as { active: boolean; text?: string };
+  } as Error;
+
+  let data = {
+    password: "",
+    username: "",
+    accessKey: "",
+    secretKey: "",
+    bucket: "",
+    admin: "",
+    endpoint: "",
+  } as RegisterOrLogin;
 
   function resetForm() {
-    password = "";
-    username = "";
+    data = {
+      password: "",
+      username: "",
+      accessKey: "",
+      secretKey: "",
+      bucket: "",
+      admin: "",
+      endpoint: "",
+    };
   }
 
   async function auth() {
-    if (!password || !username) {
-      return (error = { active: true, text: `Введите ${!username ? "имя пользователя" : "пароль"}!` });
+    if (!data.password || !data.username) {
+      return (error = { error: true, text: `Введите ${!data.username ? "имя пользователя" : "пароль"}!` });
     }
-
-    error.active = false;
 
     const apiRequest = await fetch("/api/join", {
       method: "POST",
@@ -34,101 +57,98 @@
         "Content-Type": "application/json",
       },
 
-      body: JSON.stringify({ password, username, admin, accessKey, secretKey, bucket, endpoint }),
+      body: JSON.stringify({ ...data }),
     });
 
-    const response = await apiRequest.json();
-
-    if (response.error) {
-      error.active = true;
-      error.text = response?.text ?? "Неизвестная ошибка!";
-    } else {
-      cookie.set("token", response.token, { expires: new Date(Date.now() + 1000 * 60 * 60 * 24), path: "/" });
-
-      window.location.href = "/";
-    }
+    const response = (await apiRequest.json()) as Response & Error;
 
     resetForm();
+
+    if (response.error) {
+      error.error = true;
+      error.text = response?.text ?? "Неизвестная ошибка!";
+    } else {
+      error.error = false;
+      if (isRegister) return toast.push(`Пользователь ${data.username} создан!`, toastInfo);
+
+      return (window.location.href = "/");
+    }
   }
 </script>
 
-<div class="hero min-h-screen bg-base-300 select-none">
-  <div class="hero-content text-center">
-    <div class="card rounded sm:w-96 bg-base-100 shadow-xl">
+<div class="hero min-h-screen select-none">
+  <div class="hero-content w-full text-center">
+    <div class="card rounded w-full md:w-96 bg-base-300 shadow-xl">
       <div class="card-body">
         <h2 class="text-xl outline-none text-center">{isRegister ? "Регистрация" : "Авторизация"}</h2>
 
-        <div class="form-control w-full">
-          <span class="label label-text">Имя пользователя</span>
-          <input type="text" bind:value={username} class="input-sm rounded input input-bordered w-full" required />
-        </div>
-
-        <div class="form-control w-full">
-          <span class="label label-text">Пароль</span>
-          <input type="password" bind:value={password} class="input-sm rounded input input-bordered w-full" required />
-        </div>
-
-        {#if isRegister}
+        <form on:submit|preventDefault={async () => await auth()}>
           <div class="form-control w-full">
-            <span class="label label-text">S3 Access Key</span>
-            <input type="text" bind:value={accessKey} class="input-sm rounded input input-bordered w-full" required />
+            <span class="label label-text">Имя пользователя</span>
+            <input type="text" bind:value={data.username} class="input-sm rounded input input-bordered w-full" />
           </div>
 
           <div class="form-control w-full">
-            <span class="label label-text">S3 Secret Key</span>
-            <input
-              type="password"
-              bind:value={secretKey}
-              class="input-sm rounded input input-bordered w-full"
-              required
-            />
+            <span class="label label-text">Пароль</span>
+            <input type="password" bind:value={data.password} class="input-sm rounded input input-bordered w-full" />
           </div>
 
-          <div class="form-control w-full">
-            <span class="label label-text">Название Bucket'a</span>
-            <input type="text" bind:value={bucket} class="input-sm rounded input input-bordered w-full" required />
-          </div>
-
-          <div class="form-control w-full">
-            <span class="label label-text">S3 Endpoint</span>
-            <input type="text" bind:value={endpoint} class="input-sm rounded input input-bordered w-full" required />
-          </div>
-
-          <div class="form-control w-full">
-            <span class="label label-text">Админ-пароль</span>
-            <input type="password" bind:value={admin} class="input-sm rounded input input-bordered w-full" required />
-          </div>
-        {/if}
-
-        {#if error.active}
-          <div class="mt-5 alert rounded alert-error alert-sm shadow-lg">
-            <div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="stroke-current flex-shrink-0 h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                ><path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-                /></svg
-              >
-              <span>{error.text}</span>
+          {#if isRegister}
+            <div class="form-control w-full">
+              <span class="label label-text">S3 Access Key</span>
+              <input type="text" bind:value={data.accessKey} class="input-sm rounded input input-bordered w-full" />
             </div>
-          </div>
-        {/if}
-        <div class="card-actions justify-end mt-5">
-          <button
-            on:click={() => (isRegister ? (isRegister = false) : (isRegister = true))}
-            class="btn rounded btn-outline btn-error btn-sm">{isRegister ? "Отмена" : "Регистрация"}</button
-          >
 
-          <button on:click={async () => await auth()} class="btn rounded btn-outline btn-access btn-sm"
-            >{isRegister ? "Создать" : "Войти"}</button
-          >
-        </div>
+            <div class="form-control w-full">
+              <span class="label label-text">S3 Secret Key</span>
+              <input type="password" bind:value={data.secretKey} class="input-sm rounded input input-bordered w-full" />
+            </div>
+
+            <div class="form-control w-full">
+              <span class="label label-text">Название Bucket'a</span>
+              <input type="text" bind:value={data.bucket} class="input-sm rounded input input-bordered w-full" />
+            </div>
+
+            <div class="form-control w-full">
+              <span class="label label-text">S3 Endpoint</span>
+              <input type="text" bind:value={data.endpoint} class="input-sm rounded input input-bordered w-full" />
+            </div>
+
+            <div class="form-control w-full">
+              <span class="label label-text">Админ-пароль</span>
+              <input type="password" bind:value={data.admin} class="input-sm rounded input input-bordered w-full" />
+            </div>
+          {/if}
+
+          {#if error.error}
+            <div class="mt-5 alert rounded alert-error alert-sm shadow-lg">
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="stroke-current flex-shrink-0 h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  ><path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  /></svg
+                >
+                <span>{error.text}</span>
+              </div>
+            </div>
+          {/if}
+
+          <div class="card-actions justify-end mt-5">
+            <button class="btn rounded btn-outline btn-access btn-sm">{isRegister ? "Создать" : "Войти"}</button>
+
+            <button
+              on:click={() => (isRegister ? (isRegister = false) : (isRegister = true))}
+              class="btn rounded btn-outline btn-error btn-sm">{isRegister ? "Отмена" : "Регистрация"}</button
+            >
+          </div>
+        </form>
       </div>
     </div>
   </div>
