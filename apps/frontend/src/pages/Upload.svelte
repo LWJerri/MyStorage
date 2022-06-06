8ffb0fb2f730f6b2705ee8f0aafd014b8b54c4bf
LWJerri <CLWJerri@yandex.ua>
2022-06-06 16:07:28 +0000

    Optimize code structure, some functions move in components

--- apps/frontend/src/pages/Upload.svelte
+++ apps/frontend/src/pages/Upload.svelte
@@ -1,32 +1,73 @@
 <script lang="ts">
-  import cookie from "js-cookie";
+  import { toast } from "@zerodevx/svelte-toast";
+  import { toastError, toastInfo } from "../helpers/toasts";
   import { onMount } from "svelte";
   import Navbar from "../components/Navbar.svelte";
 
-  const token = cookie.get("token");
+  let files: FileList;
+  let isUploading: boolean = false;
 
-  onMount(() => {
-    if (!cookie.get("token")) {
-      document.location.href = "/join";
+  async function uploadFiles() {
+    isUploading = true;
+    toast.push(`Загрузка файлов, пожалуйста, подождите...`, { ...toastInfo, duration: 10000 });
+
+    const data = new FormData();
+
+    for (const file of files) {
+      data.append("files[]", file, file.name);
     }
+
+    const apiRequest = await fetch("/api/file", {
+      method: "POST",
+      body: data,
+    });
+
+    const response = await apiRequest.json();
+
+    if (response.error) {
+      isUploading = false;
+
+      return toast.push(`Произошла ошибка во время загрузки файлов!`, toastError);
+    } else {
+      return (document.location.href = "/");
+    }
+  }
+
+  async function getMember() {
+    const apiRequest = await fetch("/api/me", {
+      method: "GET",
+    });
+
+    const response = await apiRequest.json();
+
+    if (response.error) return (document.location.href = "/join");
+  }
+
+  onMount(async () => {
+    await getMember();
   });
 </script>
 
 <div>
   <Navbar />
 
-  <form
-    class="my-5"
-    action="/api/file?token={token}"
-    method="post"
-    accept-charset="UTF-8"
-    enctype="multipart/form-data"
-  >
+  <form on:submit|preventDefault={async () => await uploadFiles()} class="my-5">
     <div class="flex justify-center px-1">
       <div class="w-96">
-        <input class="form-control block w-full text-white rounded my-1" type="file" name="files" multiple required />
+        <input
+          bind:files
+          class="form-control block w-full text-white rounded my-1"
+          type="file"
+          name="files"
+          multiple
+          required
+        />
 
-        <button type="submit" class="btn btn-sm w-full my-1 btn-outline btn-error rounded">Загрузить</button>
+        <button
+          type="submit"
+          class="btn btn-sm w-full my-1 btn-outline btn-error rounded {isUploading ? 'loading' : ''}"
+          disabled={isUploading}>{isUploading ? "Загрузка" : "Загрузить"}</button
+        >
       </div>
     </div>
   </form>
