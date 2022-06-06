73a05d7f37f29e807d7b9dff62f507fa82db993b
LWJerri <CLWJerri@yandex.ua>
2022-06-06 16:06:14 +0000

    Add `toastInfo`, `toastWarn` and `toastError` themes

---
+++ apps/frontend/src/helpers/toasts.ts
@@ -0,0 +1,20 @@
+export const toastError = {
+  theme: {
+    "--toastBackground": "#F56565",
+    "--toastBarBackground": "#C53030",
+  },
+};
+
+export const toastWarn = {
+  theme: {
+    "--toastBackground": "#336ecc",
+    "--toastBarBackground": "#265aad",
+  },
+};
+
+export const toastInfo = {
+  theme: {
+    "--toastBackground": "#48BB78",
+    "--toastBarBackground": "#2F855A",
+  },
+};
