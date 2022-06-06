173cc00e143ac5e498662b3479924576261563c8
LWJerri <CLWJerri@yandex.ua>
2022-06-06 17:38:04 +0000

    Optimize upload feature

--- apps/backend/src/routes/upload.ts
+++ apps/backend/src/routes/upload.ts
@@ -1,7 +1,7 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { prisma } from "../app";
 import { getS3 } from "../helpers/s3";
-import stream from "stream";
+import stream, { Stream } from "stream";
 import { ManagedUpload } from "aws-sdk/clients/s3";
 
 export async function upload(req: FastifyRequest & { body: { files: any } }, res: FastifyReply) {
@@ -10,40 +10,32 @@ export async function upload(req: FastifyRequest & { body: { files: any } }, res
     const uploadS3 = await getS3(req.user.member_id);
 
     for await (const part of req.files()) {
-      part.file.pipe(await uploadFromStream());
+      const params = {
+        Bucket: member.bucket,
+        Key: `${Date.now()}-${part.filename}`,
+        Body: part.file,
+        ACL: "public-read",
+        ContentType: `${part.mimetype}; charset=utf-8`,
+      };
 
-      async function uploadFromStream() {
-        const pass = new stream.PassThrough();
+      const s3Response = await uploadS3.upload(params).promise();
 
-        const params = {
+      const size = await uploadS3
+        .headObject({
           Bucket: member.bucket,
-          Key: `${Date.now()}-${part.filename}`,
-          Body: pass,
-          ACL: "public-read",
-          ContentType: `${part.mimetype}; charset=utf-8`,
-        };
+          Key: s3Response.Key,
+        })
+        .promise();
 
-        uploadS3.upload(params, async function (err: Error, data: ManagedUpload.SendData) {
-          const size = await uploadS3
-            .headObject({
-              Bucket: member.bucket,
-              Key: data.Key,
-            })
-            .promise();
-
-          await prisma.upload.create({
-            data: {
-              name: part.filename,
-              url: data.Location.replace("http://", "https://"),
-              key: data.Key,
-              memberID: member.id,
-              size: size.ContentLength,
-            },
-          });
-        });
-
-        return pass;
-      }
+      await prisma.upload.create({
+        data: {
+          name: part.filename,
+          url: s3Response.Location.replace("http://", "https://"),
+          key: s3Response.Key,
+          memberID: member.id,
+          size: size.ContentLength,
+        },
+      });
     }
 
     return await res.status(200).send({ error: false });
