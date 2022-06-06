27df11ed8bc390618e0f44c0fa794a8d084832d6
LWJerri <CLWJerri@yandex.ua>
2022-06-06 15:25:09 +0000

    Optimize auth middleware

--- apps/backend/src/middleware/auth.ts
+++ apps/backend/src/middleware/auth.ts
@@ -1,15 +1,15 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { verify } from "jsonwebtoken";
 
-export const verifyData = async (req: FastifyRequest & { query: { token?: string } }, res: FastifyReply, next) => {
-  const token = req.query?.token || req.headers["authorization"];
+export const verifyData = async (req: FastifyRequest, res: FastifyReply, next) => {
+  const token = req.cookies?.token;
 
   if (!token) return await res.status(403).send({ error: true, text: "Предоставьте валидный токен!" });
 
   try {
-    const response = verify(token.replace("Bearer ", ""), process.env.TOKEN_KEY) as any;
+    const response = verify(token, process.env.TOKEN_KEY) as any;
     req.user = response;
   } catch (err) {
-    return await res.status(403).send({ error: true, text: "Невалидный токен" });
+    return await res.status(403).send({ error: true, text: "Невалидный токен!" });
   }
 };
