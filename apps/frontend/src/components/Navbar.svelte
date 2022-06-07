b990a8fcad805855fad922080a9709ea1c44209f
LWJerri <CLWJerri@yandex.ua>
2022-06-07 23:55:29 +0000

    Some changes in frontend

--- apps/frontend/src/components/Navbar.svelte
+++ apps/frontend/src/components/Navbar.svelte
@@ -53,10 +53,12 @@
 
 <label for="upload" class="modal cursor-pointer">
   <label class="modal-box relative" for="">
-    <label for="upload" class="btn btn-sm btn-circle absolute right-2 top-2">X</label>
+    <label for="upload" class="btn btn-sm btn-circle absolute right-2 top-2"
+      ><svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
+        ><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg
+      ></label
+    >
 
     <MemberUpload {member} />
-
-    <button for="upload" class="flex justify-center btn btn-sm btn-outline btn-wide btn-error">Закрыть</button>
   </label>
 </label>
--- apps/frontend/src/pages/Main.svelte
+++ apps/frontend/src/pages/Main.svelte
@@ -197,7 +197,11 @@
 <input type="checkbox" id="delete_files" class="modal-toggle" />
 <label for="delete_files" class="modal modal-bottom md:modal-middle cursor-pointer">
   <label class="modal-box relative rounded" for="">
-    <label for="delete_files" class="btn btn-sm btn-circle absolute right-2 top-2">X</label>
+    <label for="delete_files" class="btn btn-sm btn-circle absolute right-2 top-2"
+      ><svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
+        ><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg
+      ></label
+    >
     <h3 class="text-lg font-bold">Подтвердите удаление</h3>
     <p class="py-4">
       <span class="font-bold">Внимание!</span> Данное действие нельзя отменить, все файлы будут безвозвратно удалены с S3
