9726479f44b1148e24d5f7ae337b0918870d74cd
LWJerri <CLWJerri@yandex.ua>
2022-06-10 13:30:56 +0000

    Add `isLoading` and rewrite interfaces

--- apps/frontend/src/pages/Join.svelte
+++ apps/frontend/src/pages/Join.svelte
@@ -1,21 +1,8 @@
 <script lang="ts">
   import { toast } from "@zerodevx/svelte-toast";
+  import type { RegisterOrLogin, Error } from "../helpers/interfaces";
+  import { isLoading, _ } from "svelte-i18n";
   import { toastInfo } from "../helpers/toasts";
-
-  interface RegisterOrLogin {
-    password: string;
-    username: string;
-    accessKey: string;
-    secretKey: string;
-    bucket: string;
-    admin: string;
-    endpoint: string;
-  }
-
-  interface Error {
-    error: boolean;
-    text?: string;
-  }
 
   let isRegister: boolean = false;
 
@@ -48,7 +35,12 @@
 
   async function auth() {
     if (!data.password || !data.username) {
-      return (error = { error: true, text: `Введите ${!data.username ? "имя пользователя" : "пароль"}!` });
+      return (error = {
+        error: true,
+        text: $_("join.noCredentials", {
+          values: { data: !data.username ? $_("username").toLowerCase() : $_("password").toLowerCase() },
+        }),
+      });
     }
 
     const apiRequest = await fetch("/api/join", {
@@ -66,10 +58,10 @@
 
     if (response.error) {
       error.error = true;
-      error.text = response?.text ?? "Неизвестная ошибка!";
+      error.text = response?.text ?? $_("error.unknown");
     } else {
       error.error = false;
-      if (isRegister) return toast.push(`Пользователь ${data.username} создан!`, toastInfo);
+      if (isRegister) return toast.push($_("join.created", { values: { user: data.username } }), toastInfo);
 
       return (window.location.href = "/");
     }
@@ -78,78 +70,93 @@
 
 <div class="hero min-h-screen select-none">
   <div class="hero-content w-full text-center">
-    <div class="card rounded w-full md:w-96 bg-base-300 shadow-xl">
-      <div class="card-body">
-        <h2 class="text-xl outline-none text-center">{isRegister ? "Регистрация" : "Авторизация"}</h2>
-
-        <form on:submit|preventDefault={async () => await auth()}>
-          <div class="form-control w-full">
-            <span class="label label-text">Имя пользователя</span>
-            <input type="text" bind:value={data.username} class="input-sm rounded input input-bordered w-full" />
-          </div>
-
-          <div class="form-control w-full">
-            <span class="label label-text">Пароль</span>
-            <input type="password" bind:value={data.password} class="input-sm rounded input input-bordered w-full" />
-          </div>
-
-          {#if isRegister}
-            <div class="form-control w-full">
-              <span class="label label-text">S3 Access Key</span>
-              <input type="text" bind:value={data.accessKey} class="input-sm rounded input input-bordered w-full" />
-            </div>
-
-            <div class="form-control w-full">
-              <span class="label label-text">S3 Secret Key</span>
-              <input type="password" bind:value={data.secretKey} class="input-sm rounded input input-bordered w-full" />
-            </div>
-
-            <div class="form-control w-full">
-              <span class="label label-text">Название Bucket'a</span>
-              <input type="text" bind:value={data.bucket} class="input-sm rounded input input-bordered w-full" />
-            </div>
-
-            <div class="form-control w-full">
-              <span class="label label-text">S3 Endpoint</span>
-              <input type="text" bind:value={data.endpoint} class="input-sm rounded input input-bordered w-full" />
-            </div>
-
-            <div class="form-control w-full">
-              <span class="label label-text">Админ-пароль</span>
-              <input type="password" bind:value={data.admin} class="input-sm rounded input input-bordered w-full" />
-            </div>
-          {/if}
-
-          {#if error.error}
-            <div class="mt-5 alert rounded alert-error alert-sm shadow-lg">
-              <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="stroke-current flex-shrink-0 h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  ><path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-                  /></svg
-                >
-                <span>{error.text}</span>
-              </div>
-            </div>
-          {/if}
-
-          <div class="card-actions justify-end mt-5">
-            <button class="btn rounded btn-outline btn-access btn-sm">{isRegister ? "Создать" : "Войти"}</button>
-
-            <button
-              on:click={() => (isRegister ? (isRegister = false) : (isRegister = true))}
-              class="btn rounded btn-outline btn-error btn-sm">{isRegister ? "Отмена" : "Регистрация"}</button
-            >
-          </div>
-        </form>
+    {#if $isLoading}
+      <div class="card rounded w-full md:w-96 bg-base-300 shadow-xl">
+        <div class="card-body">
+          <h2 class="text-xl outline-none text-center">{$_("page_loading")}</h2>
+        </div>
       </div>
-    </div>
+    {:else}
+      <div class="card rounded w-full md:w-96 bg-base-300 shadow-xl">
+        <div class="card-body">
+          <h2 class="text-xl outline-none text-center">{isRegister ? $_("join.register") : $_("join.login")}</h2>
+
+          <form on:submit|preventDefault={async () => await auth()}>
+            <div class="form-control w-full">
+              <span class="label label-text">{$_("username")}</span>
+              <input type="text" bind:value={data.username} class="input-sm rounded input input-bordered w-full" />
+            </div>
+
+            <div class="form-control w-full">
+              <span class="label label-text">{$_("password")}</span>
+              <input type="password" bind:value={data.password} class="input-sm rounded input input-bordered w-full" />
+            </div>
+
+            {#if isRegister}
+              <div class="form-control w-full">
+                <span class="label label-text">{$_("accessKey")}</span>
+                <input type="text" bind:value={data.accessKey} class="input-sm rounded input input-bordered w-full" />
+              </div>
+
+              <div class="form-control w-full">
+                <span class="label label-text">{$_("secretKey")}</span>
+                <input
+                  type="password"
+                  bind:value={data.secretKey}
+                  class="input-sm rounded input input-bordered w-full"
+                />
+              </div>
+
+              <div class="form-control w-full">
+                <span class="label label-text">{$_("s3Bucket")}</span>
+                <input type="text" bind:value={data.bucket} class="input-sm rounded input input-bordered w-full" />
+              </div>
+
+              <div class="form-control w-full">
+                <span class="label label-text">{$_("s3Endpoint")}</span>
+                <input type="text" bind:value={data.endpoint} class="input-sm rounded input input-bordered w-full" />
+              </div>
+
+              <div class="form-control w-full">
+                <span class="label label-text">{$_("adminPassword")}</span>
+                <input type="password" bind:value={data.admin} class="input-sm rounded input input-bordered w-full" />
+              </div>
+            {/if}
+
+            {#if error.error}
+              <div class="mt-5 alert rounded alert-error alert-sm shadow-lg">
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="stroke-current flex-shrink-0 h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    ><path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                    /></svg
+                  >
+                  <span>{error.text}</span>
+                </div>
+              </div>
+            {/if}
+
+            <div class="card-actions justify-end mt-5">
+              <button class="btn rounded btn-outline btn-access btn-sm"
+                >{isRegister ? $_("join.create") : $_("join.enter")}</button
+              >
+
+              <button
+                on:click={() => (isRegister ? (isRegister = false) : (isRegister = true))}
+                class="btn rounded btn-outline btn-error btn-sm"
+                >{isRegister ? $_("join.cancel") : $_("join.register")}</button
+              >
+            </div>
+          </form>
+        </div>
+      </div>
+    {/if}
   </div>
 </div>
--- apps/frontend/src/pages/Main.svelte
+++ apps/frontend/src/pages/Main.svelte
@@ -7,32 +7,8 @@
   import MemberControl from "../components/MemberControl.svelte";
   import MemberUploads from "../components/MemberUploads.svelte";
   import { files } from "../helpers/store";
-
-  interface Response {
-    error: boolean;
-    uploads: Array<{ id: string; createdAt: Date; size: number; name: string; url: string; key: string }>;
-    nextPage: boolean;
-  }
-
-  interface Member {
-    error: boolean;
-    text?: string;
-    member: {
-      id: string;
-      createdAt: Date;
-      username: string;
-      accessKey: string;
-      secretKey: string;
-      bucket: string;
-      endpoint: string;
-      showPreview: boolean;
-      maxGB: number;
-    };
-    uploads: {
-      size: number;
-      count: number;
-    };
-  }
+  import type { Member, Response } from "../helpers/interfaces";
+  import { isLoading, _ } from "svelte-i18n";
 
   $: response = {} as Response;
   $: member = {} as Member;
@@ -42,15 +18,7 @@
   let startDeleting: boolean = false;
   let page: number = 1;
 
-  let newInfo = {} as {
-    username: string;
-    password: string;
-    accessKey: string;
-    secretKey: string;
-    bucket: string;
-    endpoint: string;
-    maxGB: number;
-  };
+  let newInfo = {} as Member["member"] & { password?: string };
 
   async function getFiles() {
     const apiRequest = await (await fetch(`/api/file?page=${page}&key=${search ?? ""}`, { method: "GET" })).json();
@@ -67,7 +35,7 @@
     const response = (await apiRequest.json()) as Member;
 
     if (response.error) {
-      toast.push(response?.text ?? `Произошла ошибка при получении данных о пользователе!`, toastError);
+      toast.push(response?.text ?? $_("error.memberGet"), toastError);
     } else {
       member = response;
       isAuth = true;
@@ -84,11 +52,11 @@
     const response = await apiRequest.json();
 
     if (response.error) {
-      toast.push(response?.text ?? `Произошла ошибка при удалении файлов!`, toastError);
+      toast.push(response?.text ?? $_("error.fileDelete"), toastError);
     } else {
       await getFiles();
 
-      toast.push(`Удалено ${response.count} файлов!`, toastInfo);
+      toast.push($_("deleted", { values: { count: response.count } }), toastInfo);
     }
 
     startDeleting = false;
@@ -101,99 +69,113 @@
       document.location.href = "/join";
     }
 
+    window.localStorage.setItem("lang", member.member.language);
+
     await getFiles();
   });
 </script>
 
-<div class="bg-base-200">
-  <Navbar {member} />
-
-  <div class="grid gap-1 mt-5 grid-cols-1 md:grid-cols-2">
-    <MemberInfo {member} {newInfo} />
-
-    <MemberControl {member} {page} />
+{#if $isLoading}
+  <div class="hero min-h-screen select-none">
+    <div class="hero-content w-full text-center">
+      <div class="card rounded w-full md:w-96 bg-base-300 shadow-xl">
+        <div class="card-body">
+          <h2 class="text-xl outline-none text-center">{$_("page_loading")}</h2>
+        </div>
+      </div>
+    </div>
   </div>
+{:else}
+  <div>
+    <Navbar {member} />
 
-  {#if response?.uploads?.length > 0 && isAuth}
-    <div class="flex justify-center my-2 space-x-1">
-      <button
-        on:click={async () => {
-          page--;
-          await getFiles();
-        }}
-        class="btn btn-sm btn-success btn-outline rounded"
-        disabled={page == 1 ? true : false}>Предыдущая</button
-      >
+    <div class="grid gap-1 mt-5 grid-cols-1 md:grid-cols-2">
+      <MemberInfo {member} {newInfo} />
 
-      <button
-        on:click={async () => {
-          page++;
-          await getFiles();
-        }}
-        disabled={response.nextPage ? false : true}
-        class="btn btn-sm btn-success btn-outline rounded">Следующая</button
-      >
+      <MemberControl {member} {page} />
     </div>
 
-    <MemberUploads {member} />
-
-    <div class="flex justify-center my-2 space-x-1">
-      <button
-        on:click={async () => {
-          page--;
-          await getFiles();
-        }}
-        class="btn btn-sm btn-success btn-outline rounded"
-        disabled={page == 1 ? true : false}>Предыдущая</button
-      >
-
-      <button
-        on:click={async () => {
-          page++;
-          await getFiles();
-        }}
-        disabled={response.nextPage ? false : true}
-        class="btn btn-sm btn-success btn-outline rounded">Следующая</button
-      >
-    </div>
-  {:else if response.error}
-    <div class="alert alert-error rounded mt-5">
-      <div>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="stroke-current flex-shrink-0 h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          ><path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-          /></svg
+    {#if response?.uploads?.length > 0 && isAuth}
+      <div class="flex justify-center my-2 space-x-1">
+        <button
+          on:click={async () => {
+            page--;
+            await getFiles();
+          }}
+          class="btn btn-sm btn-success btn-outline rounded"
+          disabled={page == 1 ? true : false}>{$_("pagination.prev")}</button
         >
-        <span>Произошла ошибка при загрузке файлов!</span>
-      </div>
-    </div>
-  {:else}
-    <div class="alert alert-warning rounded mt-5">
-      <div>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="stroke-current flex-shrink-0 h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          ><path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-          /></svg
+
+        <button
+          on:click={async () => {
+            page++;
+            await getFiles();
+          }}
+          disabled={response.nextPage ? false : true}
+          class="btn btn-sm btn-success btn-outline rounded">{$_("pagination.next")}</button
         >
-        <span class="select-none">Список загруженных файлов пуст!</span>
       </div>
-    </div>
-  {/if}
-</div>
+
+      <MemberUploads {member} />
+
+      <div class="flex justify-center my-2 space-x-1">
+        <button
+          on:click={async () => {
+            page--;
+            await getFiles();
+          }}
+          class="btn btn-sm btn-success btn-outline rounded"
+          disabled={page == 1 ? true : false}>{$_("pagination.prev")}</button
+        >
+
+        <button
+          on:click={async () => {
+            page++;
+            await getFiles();
+          }}
+          disabled={response.nextPage ? false : true}
+          class="btn btn-sm btn-success btn-outline rounded">{$_("pagination.next")}</button
+        >
+      </div>
+    {:else if response.error}
+      <div class="alert alert-error rounded mt-5">
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="stroke-current flex-shrink-0 h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            ><path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+            /></svg
+          >
+          <span>{$_("error.loadFiles")}</span>
+        </div>
+      </div>
+    {:else}
+      <div class="alert alert-warning rounded mt-5">
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="stroke-current flex-shrink-0 h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            ><path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+            /></svg
+          >
+          <span class="select-none">{$_("noFiles")}</span>
+        </div>
+      </div>
+    {/if}
+  </div>
+{/if}
 
 <input type="checkbox" id="delete_files" class="modal-toggle" />
 <label for="delete_files" class="modal modal-bottom md:modal-middle cursor-pointer">
@@ -203,16 +185,13 @@
         ><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg
       ></label
     >
-    <h3 class="text-lg font-bold">Подтвердите удаление</h3>
-    <p class="py-4">
-      <span class="font-bold">Внимание!</span> Данное действие нельзя отменить, все файлы будут безвозвратно удалены с S3
-      и базы данных.
-    </p>
+    <h3 class="text-lg font-bold">{$_("deleteModal.title")}</h3>
+    <p class="py-4">{$_("deleteModal.description")}</p>
 
     <button
       on:click={async () => await forceDeleteFile()}
       class="btn btn-error btn-outline w-full rounded {startDeleting ? 'loading' : ''}"
-      disabled={startDeleting}>{startDeleting ? "Удаление" : "Удалить"}</button
+      disabled={startDeleting}>{startDeleting ? $_("deleteModal.deleting") : $_("delete")}</button
     >
   </label>
 </label>
