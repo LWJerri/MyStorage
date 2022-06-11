f8bea35743e9e380f5586db369d875c0c17983d3
LWJerri <CLWJerri@yandex.ua>
2022-06-11 14:18:46 +0000

    Add `Ukranian` localization

--- apps/frontend/src/i18n.ts
+++ apps/frontend/src/i18n.ts
@@ -1,8 +1,10 @@
 import { addMessages, init } from "svelte-i18n";
 
 import russian from "../locales/russian.json";
+import ukrainian from "../locales/ukrainian.json";
 
 addMessages("russian", russian);
+addMessages("ukrainian", ukrainian);
 
 const memberLang = window.localStorage.getItem("lang");
 
