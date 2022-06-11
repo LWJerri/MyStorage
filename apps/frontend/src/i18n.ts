3309f36948752eafb7a3b01e914f8b988de6f238
LWJerri <CLWJerri@yandex.ua>
2022-06-11 14:33:47 +0000

    Add `English` localization

--- apps/frontend/src/i18n.ts
+++ apps/frontend/src/i18n.ts
@@ -2,9 +2,11 @@ import { addMessages, init } from "svelte-i18n";
 
 import russian from "../locales/russian.json";
 import ukrainian from "../locales/ukrainian.json";
+import english from "../locales/english.json";
 
 addMessages("russian", russian);
 addMessages("ukrainian", ukrainian);
+addMessages("english", english);
 
 const memberLang = window.localStorage.getItem("lang");
 
