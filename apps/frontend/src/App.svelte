f9b795809b0977688488d4dd86465d378d3288e8
LWJerri <loginmanx@gmail.com>
2022-07-02 22:55:05 +0000

    Remove `Svroller`

--- apps/frontend/src/App.svelte
+++ apps/frontend/src/App.svelte
@@ -1,19 +1,16 @@
 <script lang="ts">
   import { Route, Router } from "svelte-navigator";
   import { SvelteToast } from "@zerodevx/svelte-toast";
-  import { Svroller } from "svrollbar";
 
   import Main from "./pages/Main.svelte";
   import Join from "./pages/Join.svelte";
 </script>
 
-<Svroller width="100vw" height="100vh">
-  <Router>
-    <main>
-      <SvelteToast />
+<Router>
+  <main>
+    <SvelteToast />
 
-      <Route path="/join" component={Join} />
-      <Route component={Main} />
-    </main>
-  </Router>
-</Svroller>
+    <Route path="/join" component={Join} />
+    <Route component={Main} />
+  </main>
+</Router>
